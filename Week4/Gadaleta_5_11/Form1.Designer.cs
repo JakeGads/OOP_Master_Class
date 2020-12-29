772a55ad360d9792335ffd0f71de406db6fe5011
Jake Gadaleta <jake.gads@gmail.com>
2020-12-28 21:27:24 -0500

    another one

--- Week4/Gadaleta_5_11/Form1.Designer.cs
+++ Week4/Gadaleta_5_11/Form1.Designer.cs
@@ -29,13 +29,81 @@ namespace Gadaleta_5_11
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Guess_box = new System.Windows.Forms.TextBox();
+            this.Submit_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Hint = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guess";
+            // 
+            // Guess_box
+            // 
+            this.Guess_box.Location = new System.Drawing.Point(193, 27);
+            this.Guess_box.Name = "Guess_box";
+            this.Guess_box.Size = new System.Drawing.Size(100, 23);
+            this.Guess_box.TabIndex = 1;
+            // 
+            // Submit_btn
+            // 
+            this.Submit_btn.Location = new System.Drawing.Point(203, 56);
+            this.Submit_btn.Name = "Submit_btn";
+            this.Submit_btn.Size = new System.Drawing.Size(75, 23);
+            this.Submit_btn.TabIndex = 2;
+            this.Submit_btn.Text = "submit";
+            this.Submit_btn.UseVisualStyleBackColor = true;
+            this.Submit_btn.Click += new System.EventHandler(this.Submit_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hint";
+            // 
+            // Hint
+            // 
+            this.Hint.AutoSize = true;
+            this.Hint.Location = new System.Drawing.Point(185, 156);
+            this.Hint.Name = "Hint";
+            this.Hint.Size = new System.Drawing.Size(201, 15);
+            this.Hint.TabIndex = 4;
+            this.Hint.Text = "Hit Submit to guess";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(512, 208);
+            this.Controls.Add(this.Hint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Submit_btn);
+            this.Controls.Add(this.Guess_box);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Guess_box;
+        private System.Windows.Forms.Button Submit_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Hint;
     }
 }
 
--- Week4/Gadaleta_5_11/Form1.cs
+++ Week4/Gadaleta_5_11/Form1.cs
@@ -12,10 +12,33 @@ namespace Gadaleta_5_11
 {
     public partial class Form1 : Form
     {
+        int count = 0;
+        int rand = 0;
         public Form1()
         {
             InitializeComponent();
+            rand = new Random().Next(100);
         }
 
+        private void Submit_btn_Click(object sender, EventArgs e)
+        {
+            int guess = int.Parse(this.Guess_box.Text);
+            if(guess < rand)
+            {
+                this.Hint.Text = $"{guess} is too low";
+                
+
+            }
+            else if (guess > rand)
+            {
+                this.Hint.Text = $"{guess} is too high";
+              
+            }
+            else
+            {
+                this.Hint.Text = $"You got it right!!\nIt took {count} tries";
+            }
+            count += 1;
+        }
     }
 }
