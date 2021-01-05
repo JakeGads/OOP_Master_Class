963cdbc4716e3db6e83bc1da53e1db2ffffaefdf
Jake Gadaleta <jake.gads@gmail.com>
2021-01-05 18:12:16 -0500

    midterm like 90% there

--- Week06 (Midterm)/Midterm/Form1.Designer.cs
+++ Week06 (Midterm)/Midterm/Form1.Designer.cs
@@ -29,30 +29,32 @@ namespace Midterm
         /// </summary>
         private void InitializeComponent()
         {
-            this.Setup_Box = new System.Windows.Forms.GroupBox();
+            this.Setup_gb = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.Submit_Btn = new System.Windows.Forms.Button();
             this.Add_Bill_Btn = new System.Windows.Forms.Button();
             this.Add_Cost_Tf = new System.Windows.Forms.TextBox();
             this.Month_cb = new System.Windows.Forms.ComboBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.Setup_Box.SuspendLayout();
+            this.Data_gb = new System.Windows.Forms.GroupBox();
+            this.Output_gb = new System.Windows.Forms.GroupBox();
+            this.finalize_btn = new System.Windows.Forms.Button();
+            this.Setup_gb.SuspendLayout();
+            this.Data_gb.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Setup_Box
+            // Setup_gb
             // 
-            this.Setup_Box.Controls.Add(this.label1);
-            this.Setup_Box.Controls.Add(this.Submit_Btn);
-            this.Setup_Box.Controls.Add(this.Add_Bill_Btn);
-            this.Setup_Box.Controls.Add(this.Add_Cost_Tf);
-            this.Setup_Box.Controls.Add(this.Month_cb);
-            this.Setup_Box.Location = new System.Drawing.Point(13, 13);
-            this.Setup_Box.Name = "Setup_Box";
-            this.Setup_Box.Size = new System.Drawing.Size(384, 224);
-            this.Setup_Box.TabIndex = 0;
-            this.Setup_Box.TabStop = false;
-            this.Setup_Box.Text = "Setup";
+            this.Setup_gb.Controls.Add(this.label1);
+            this.Setup_gb.Controls.Add(this.Submit_Btn);
+            this.Setup_gb.Controls.Add(this.Add_Bill_Btn);
+            this.Setup_gb.Controls.Add(this.Add_Cost_Tf);
+            this.Setup_gb.Controls.Add(this.Month_cb);
+            this.Setup_gb.Location = new System.Drawing.Point(13, 13);
+            this.Setup_gb.Name = "Setup_gb";
+            this.Setup_gb.Size = new System.Drawing.Size(384, 224);
+            this.Setup_gb.TabIndex = 0;
+            this.Setup_gb.TabStop = false;
+            this.Setup_gb.Text = "Setup";
             // 
             // label1
             // 
@@ -92,6 +94,7 @@ namespace Midterm
             // 
             // Month_cb
             // 
+            this.Month_cb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.Month_cb.FormattingEnabled = true;
             this.Month_cb.Items.AddRange(new object[] {
             "January",
@@ -111,50 +114,63 @@ namespace Midterm
             this.Month_cb.Size = new System.Drawing.Size(371, 23);
             this.Month_cb.TabIndex = 0;
             // 
-            // groupBox2
+            // Data_gb
             // 
-            this.groupBox2.Location = new System.Drawing.Point(415, 13);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(373, 739);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.Data_gb.Controls.Add(this.finalize_btn);
+            this.Data_gb.Location = new System.Drawing.Point(415, 13);
+            this.Data_gb.Name = "Data_gb";
+            this.Data_gb.Size = new System.Drawing.Size(373, 739);
+            this.Data_gb.TabIndex = 1;
+            this.Data_gb.TabStop = false;
+            this.Data_gb.Text = "Data Entry";
             // 
-            // groupBox3
+            // Output_gb
             // 
-            this.groupBox3.Location = new System.Drawing.Point(13, 243);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(384, 509);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "groupBox3";
+            this.Output_gb.Location = new System.Drawing.Point(13, 243);
+            this.Output_gb.Name = "Output_gb";
+            this.Output_gb.Size = new System.Drawing.Size(384, 509);
+            this.Output_gb.TabIndex = 2;
+            this.Output_gb.TabStop = false;
+            this.Output_gb.Text = " Final Info";
+            // 
+            // finalize_btn
+            // 
+            this.finalize_btn.Location = new System.Drawing.Point(153, 710);
+            this.finalize_btn.Name = "finalize_btn";
+            this.finalize_btn.Size = new System.Drawing.Size(75, 23);
+            this.finalize_btn.TabIndex = 0;
+            this.finalize_btn.Text = "Finalize";
+            this.finalize_btn.UseVisualStyleBackColor = true;
+            this.finalize_btn.Click += new System.EventHandler(this.finalize_btn_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 764);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.Setup_Box);
+            this.Controls.Add(this.Output_gb);
+            this.Controls.Add(this.Data_gb);
+            this.Controls.Add(this.Setup_gb);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Setup_Box.ResumeLayout(false);
-            this.Setup_Box.PerformLayout();
+            this.Setup_gb.ResumeLayout(false);
+            this.Setup_gb.PerformLayout();
+            this.Data_gb.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox Setup_Box;
+        private System.Windows.Forms.GroupBox Setup_gb;
         private System.Windows.Forms.Button Add_Bill_Btn;
         private System.Windows.Forms.TextBox Add_Cost_Tf;
         private System.Windows.Forms.ComboBox Month_cb;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox Data_gb;
+        private System.Windows.Forms.GroupBox Output_gb;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Submit_Btn;
+        private System.Windows.Forms.Button finalize_btn;
     }
 }
 
