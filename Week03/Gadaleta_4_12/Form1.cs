9c089b13f7de18c2f66b31f5e32253650c13dee7
Jake Gadaleta <jake.gads@gmail.com>
2021-01-16 21:17:26 -0500

    commented

--- Week03/Gadaleta_4_12/Form1.cs
+++ Week03/Gadaleta_4_12/Form1.cs
@@ -12,6 +12,7 @@ namespace Gadaleta_4_12
 {
     public partial class Form1 : Form
     {
+        // Arrays to hold class information for the datagrids
         Workshop[] workshops = new Workshop[] { new Workshop("Handling Stress", 3, 1000), new Workshop("Time Management", 3, 800), new Workshop("Supervision Skills", 3, 1500), new Workshop("How to Interview", 1, 500) };
         City[] cities = new City[] { new City("Austin", 150), new City("Chicago", 225), new City("Dallas", 175), new City("Orlando", 300), new City("Phoenix", 175), new City("Raleigh", 150) };
         public Form1()
@@ -20,39 +21,57 @@ namespace Gadaleta_4_12
             GenerateDataGridView();
         }
 
+        /// <summary>
+        /// sets up and formats cells in the data grid
+        /// </summary>
         private void GenerateDataGridView()
         {
+            // assigns the sources
             this.WorkshopDataGrid.DataSource = workshops;
             this.CityDataGrid.DataSource = cities;
 
+            // formats them to they are properly readable
             this.WorkshopDataGrid.Columns["fee"].DefaultCellStyle.Format = "c";
-            this.WorkshopDataGrid.Columns["Name"].Width += 20;
+            this.WorkshopDataGrid.Columns["Name"].Width += 20; // done on the fly becuase the data has to exist first
             this.CityDataGrid.Columns["fee"].DefaultCellStyle.Format = "c";
         }
 
-        private void calc_total()
+        /// <summary>
+        /// fires each time the selection in either text box changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DataGrid_SelectionChanged(object sender, EventArgs e)
         {
+            // grabs the first city as a defaulted value
             var city = cities[0];
             var workshop = workshops[0];
 
+            // attempts to assing by checking which cell is currently clicked
+            // if it fails to locate it will then default to the first city
             try
             {
                 city = cities[CityDataGrid.CurrentCell.RowIndex];
+            }
+            catch (NullReferenceException err)
+            {
+                Console.WriteLine(err);
+            }
+
+            try
+            {
                 workshop = workshops[WorkshopDataGrid.CurrentCell.RowIndex];
             }
-            catch (NullReferenceException e)
+            catch (NullReferenceException err)
             {
-                Console.WriteLine(e);
+                Console.WriteLine(err);
             }
-            
+
+
+            // update the text boxes
             this.CityFeeTB.Text = String.Format("${0:N2}", city.Fee);
             this.WorkshopCostTB.Text = String.Format("${0:N2}", workshop.Fee);
             this.TotalTB.Text = String.Format("${0:N2}", workshop.Fee);
-        }
-
-        private void DataGrid_SelectionChanged(object sender, EventArgs e)
-        {
-            calc_total();
         }
     }
 }
